5ef7e51ed166ff6618e5da3e5b72513dec5397b2
Holy Lovenia <holy.lovenia@gmail.com>
2024-04-12 12:36:18 +0000

    Remove old experiments' results

--- evaluation/mabl_data/dowload_data.sh
+++
@@ -1,3 +0,0 @@
-wget https://raw.githubusercontent.com/simran-khanuja/Multilingual-Fig-QA/main/langdata/su.csv
-wget https://raw.githubusercontent.com/simran-khanuja/Multilingual-Fig-QA/main/langdata/jv.csv
-wget https://raw.githubusercontent.com/simran-khanuja/Multilingual-Fig-QA/main/langdata/id.csv
