6fad5bc6ab15b13a7b34ca8e749ff73b74b9a2a2
Holy Lovenia <holy.lovenia@gmail.com>
2024-06-14 08:45:58 +0000

    Clean up

--- translationese/run.sh
+++ translationese/run.sh
@@ -1,8 +1,3 @@
-# # Main Exp
-# CUDA_VISIBLE_DEVICES=4 python main.py --model_path=FacebookAI/xlm-roberta-base --ouput_dir=./runs/ --batch_size=1024 --n_epochs=50
-# CUDA_VISIBLE_DEVICES=4 python main.py --model_path=FacebookAI/xlm-roberta-large --ouput_dir=./runs/ --batch_size=1024 --n_epochs=50
-# CUDA_VISIBLE_DEVICES=4 python main.py --model_path=microsoft/mdeberta-v3-base --ouput_dir=./runs/ --batch_size=512 --n_epochs=50
-CUDA_VISIBLE_DEVICES=4 python main.py --model_path=sentence-transformers/LaBSE --ouput_dir=./runs/labse --logging_dir=./logs/labse --batch_size=512 --n_epochs=20 --lr 5e-5
+# Fine-tuning the translationese classifier
 
-# Main Per Lang Exp
-# CUDA_VISIBLE_DEVICES=4 python main_per_lang.py --model_path=microsoft/mdeberta-v3-base --ouput_dir=./runs/per_lang_mdeberta-v3-base --batch_size=256 --n_epochs=5
+CUDA_VISIBLE_DEVICES=4 python main.py --model_path=microsoft/mdeberta-v3-base --ouput_dir=./runs/ --batch_size=512 --n_epochs=50
--- translationese/run_nlg_prompt.sh
+++ translationese/run_nlg_prompt.sh
@@ -15,25 +15,25 @@
 # # ####
 
 # # AYA-23 --> Holy
-# CUDA_VISIBLE_DEVICES=5 python main_nlg_prompt_batch.py CohereForAI/aya-23-8B 4 &
+# CUDA_VISIBLE_DEVICES=7 python main_nlg_prompt_batch.py CohereForAI/aya-23-8B 4 &
 
-# # Multilingual - BLOOMZ --> Ruochen
-# CUDA_VISIBLE_DEVICES=1 python main_nlg_prompt_batch.py bigscience/bloomz-7b1 4 &
+# Multilingual - BLOOMZ --> Ruochen
+CUDA_VISIBLE_DEVICES=0 python main_nlg_prompt_batch.py bigscience/bloomz-7b1 4 &
 
-# # Multilingual - Bactrian-X --> Ruochen
-# CUDA_VISIBLE_DEVICES=2 python main_nlg_prompt_batch.py MBZUAI/bactrian-x-llama-7b-merged 4 &
+# Multilingual - Bactrian-X --> Ruochen
+CUDA_VISIBLE_DEVICES=1 python main_nlg_prompt_batch.py MBZUAI/bactrian-x-llama-7b-merged 4 &
 
-# # Multilingual - Mistral --> Joseph
-# CUDA_VISIBLE_DEVICES=2 python main_nlg_prompt_batch.py mistralai/Mistral-7B-Instruct-v0.2 4 &
+# Multilingual - Mistral --> Joseph
+CUDA_VISIBLE_DEVICES=2 python main_nlg_prompt_batch.py mistralai/Mistral-7B-Instruct-v0.2 4 &
 
-# # Llama 3 --> Joseph
-# CUDA_VISIBLE_DEVICES=1 python main_nlg_prompt_batch.py meta-llama/Meta-Llama-3-8B-Instruct 4 &
+# Llama 3 --> Joseph
+CUDA_VISIBLE_DEVICES=6 python main_nlg_prompt_batch.py meta-llama/Meta-Llama-3-8B-Instruct 4 &
 
-# # English - Falcon --> Ruochen
-# CUDA_VISIBLE_DEVICES=3 python main_nlg_prompt_batch.py tiiuae/falcon-7b-instruct 4 &
+# English - Falcon --> Ruochen
+CUDA_VISIBLE_DEVICES=7 python main_nlg_prompt_batch.py tiiuae/falcon-7b-instruct 4 &
 
-# Bilingual Eng/Chinese - Qwen --> Holy
-CUDA_VISIBLE_DEVICES=0 python main_nlg_prompt_batch.py Qwen/Qwen-7B-Chat 2 &
+# # Bilingual Eng/Chinese - Qwen --> Holy
+# CUDA_VISIBLE_DEVICES=0 python main_nlg_prompt_batch.py Qwen/Qwen-7B-Chat 2 &
 
 # # SEA - SEA-LION --> Holy
 # CUDA_VISIBLE_DEVICES=7 python main_nlg_prompt_batch.py aisingapore/sea-lion-7b-instruct-research 8 &
@@ -41,14 +41,14 @@ CUDA_VISIBLE_DEVICES=0 python main_nlg_prompt_batch.py Qwen/Qwen-7B-Chat 2 &
 # # SEA - SeaLLMs 2.5 --> Holy/Fajri
 # CUDA_VISIBLE_DEVICES=3 python main_nlg_prompt_batch.py SeaLLMs/SeaLLM-7B-v2.5 4 &
 
-# SEA - SAILOR --> Holy
-CUDA_VISIBLE_DEVICES=1 python main_nlg_prompt_batch.py sail/Sailor-7B-Chat 8 &
+# # SEA - SAILOR --> Holy
+# CUDA_VISIBLE_DEVICES=1 python main_nlg_prompt_batch.py sail/Sailor-7B-Chat 4 &
 
 # # Indo - Cendol-Instruct LLaMA2 --> Holy/Fajri
 # CUDA_VISIBLE_DEVICES=2 python main_nlg_prompt_batch.py indonlp/cendol-llama2-7b 8 &
 
 # # Indo - Merak-7B-v4 --> Holy/Fajri
-# CUDA_VISIBLE_DEVICES=1 python main_nlg_prompt_batch.py Ichsan2895/Merak-7B-v4 16 &
+# CUDA_VISIBLE_DEVICES=6 python main_nlg_prompt_batch.py Ichsan2895/Merak-7B-v4 4 &
 
 # # Vietnamese - PhoGPT --> Gated model, never got the approval
 # CUDA_VISIBLE_DEVICES=4 python main_nlg_prompt_batch.py vinai/PhoGPT-7B5-Instruct 4 &
@@ -63,5 +63,5 @@ CUDA_VISIBLE_DEVICES=1 python main_nlg_prompt_batch.py sail/Sailor-7B-Chat 8 &
 # # Zero-Shot 13B Eng
 # ####
 
-# AYA --> Holy
-CUDA_VISIBLE_DEVICES=6 python main_nlg_prompt_batch.py CohereForAI/aya-101 2 &
+# # AYA --> Holy
+# CUDA_VISIBLE_DEVICES=6 python main_nlg_prompt_batch.py CohereForAI/aya-101 2 &
